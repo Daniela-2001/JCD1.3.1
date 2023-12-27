d1bdd26b3e6658ba3bed3c0852c8528787b31abd
Daniela Duarte <danielatavaresd@gmail.com>
2023-12-27 21:04:13 +0000

    JCD1.3.1

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿namespace jcd1._3._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem vindo a tabuada do 5!");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                int numero = 5;
+                int resultado = numero * i;
+
+                Console.WriteLine(numero + " X " + i + " = " + resultado);
+
+            }
+        }
+    }
+}
